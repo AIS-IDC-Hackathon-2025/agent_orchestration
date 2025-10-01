74ce9344dabc6d21e0b3af4395de1653b2c02bfc
Vikram Reddy <gvreddy04@gmail.com>
2025-10-01 13:58:21 +0530

    Trust Agent updates

---
+++ GateKeeper.AI.Trust.Agent/Usings.cs
@@ -0,0 +1,5 @@
+﻿global using GateKeeper.AI.Shared;
+global using GateKeeper.AI.Trust.Agent.Plugins;
+global using Microsoft.SemanticKernel;
+global using Microsoft.SemanticKernel.Agents;
+global using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
